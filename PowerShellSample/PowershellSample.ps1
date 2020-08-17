4fd998f451a6f9d5f626e85068fb56086d0f41f8
Anurag Rao <Anurag Rao@Mac-Bootcamp.fareast.corp.microsoft.com>
2020-08-17 21:59:40 +0530

    final

--- PowerShellSample/PowershellSample.ps1
+++ PowerShellSample/PowershellSample.ps1
@@ -1,12 +1,13 @@
-#========Config [Edit these with your settings]========
-$azureDevOpsOrgUrl = "https://dev.azure.com/<org>"      #change to the URL of your Azure DevOps account; NOTE: This must use HTTPS
-$aadClientId = "<aadClientId>"                          # change to your app registration's Application ID, unless you are an MSA backed account
-$redirectUri = "urn:ietf:wg:oauth:2.0:oob"              # change to your app registration's reply URI, unless you are an MSA backed account
+#=======Config [Edit these as per your settings]=======
+$azureDevOpsOrgUrl = "https://dev.azure.com/<org>"      # change to the URL of your Azure DevOps account; NOTE: This must use HTTPS
+$aadClientId = "<aadClientId>"                          # change to your AAD Application (client) ID
+$redirectUri = "urn:ietf:wg:oauth:2.0:oob"              # change to your app registration's reply URI
 $tenantId = "common"                                    # change to your org's tenant ID, else leave as is
 #======================================================
 
 #==============Constants [Leave as is]=================
 $azureDevOpsResId = "499b84ac-1321-427f-aa17-267ca6975798"
+$listProjectsApi = "_apis/projects?stateFilter=All&api-version=6.0-preview.4"
 #======================================================
 
 if (!(Get-Package ADAL.PS)) { 
@@ -15,5 +16,14 @@ if (!(Get-Package ADAL.PS)) {
 
 $authUrl = "https://login.microsoftonline.com/$tenantId"
 
-$response = Get-ADALToken -Resource $azureDevOpsResId -ClientId $aadClientId -RedirectUri $redirectUri -Authority $authUrl -PromptBehavior:Always
-$response.AccessToken | clip
+$authResponse = Get-ADALToken -Resource $azureDevOpsResId -ClientId $aadClientId -RedirectUri $redirectUri -Authority $authUrl -PromptBehavior:Always
+Write-Output "Access token expires on: $($authResponse.ExpiresOn)"
+
+$headers = @{
+    'Authorization' = "Bearer $($authResponse.AccessToken)"
+}
+
+$uri = "$azureDevOpsOrgUrl/$listProjectsApi"
+$apiResponse = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
+
+Write-Output $apiResponse.value
